53450d314974711935658e34c777e846bf9a4f6e
cbutterw <github@callumbutterworth.ca>
2025-12-10 22:19:10 -0500

    update template

--- template.py
+++ template.py
@@ -1,13 +1,16 @@
-def part_one(lines):
+def part_one(lines: list[str]) -> int:
   return 'not yet implemented'
 
 
-def part_two(lines):
+def part_two(lines: list[str]) -> int:
   return 'not yet implemented'
 
+def main() -> None:
+  with open('00.in') as file:
+    lines = [line.rstrip() for line in file]
 
-with open('00.in') as file:
-  lines = [line.rstrip() for line in file]
+  print('Part 1:', part_one(lines))
+  print('Part 2:', part_two(lines))
 
-print('Part 1:', part_one(lines))
-print('Part 2:', part_two(lines))
+if __name__ == '__main__':
+  main()
